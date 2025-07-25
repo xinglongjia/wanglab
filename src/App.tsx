b479c1b9d3e85aae846ce2b25fae68fecfb670a1
Xinglong Jia <jia.xi@husky.neu.edu>
2025-07-25 10:53:45 -0400

    add main page nav header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Team from './pages/Team'
@@ -12,18 +12,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/research" element={<Research />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="/news" element={<LabNews />} />
-      <Route path="/publications" element={<Publications />} />
-      <Route path="/resources" element={<Resources />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </Router>)
+    <div className="app">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/research" element={<Research />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/news" element={<LabNews />} />
+          <Route path="/publications" element={<Publications />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+   )
 }
 
 export default App
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 export default function Header() {
   return (
-    <nav style={{ padding: '1rem', backgroundColor: '#f8f8f8' }}>
-      <Link to="/">Home</Link> |{" "}
-      <Link to="/research">Research</Link> |{" "}
-      <Link to="/team">Team</Link> |{" "}
-      <Link to="/news">Lab News</Link> |{" "}
-      <Link to="/publications">Publications</Link> |{" "}
-      <Link to="/resources">Resources</Link> |{" "}
-      <Link to="/contact">Contact</Link>
-    </nav>
+    <header className="header">
+      <div className="title">
+        <strong>WANG LAB</strong> - <span className="initials">iPanCare</span>
+        <br />
+        <span className="subtitle">
+          <span className="initials">I</span>ntegrative <span className="initials">Pan</span>
+          creatic <span className="initials">Ca</span>ncer <span className="initials">R</span>esearch</span>
+      </div>
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/research">Research</Link>
+        <Link to="/team">Team</Link>
+        <Link to="/news">Lab News</Link>
+        <Link to="/publications">Publications</Link>
+        <Link to="/resources">Resources</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
   );
 }
